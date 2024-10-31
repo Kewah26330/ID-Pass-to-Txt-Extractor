a366f3c9b5921d8a90f88506834f05fd77aa39ff
Kewah26330 <kewah26330@gmail.com>
2024-10-31 13:33:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28660719
-api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6990313439"
+api_id = 23551823
+api_hash = "36727c8db0a63181fe9963f605cc4ef4"
+bot_token = "7447246036:AAH7Qb5bXb9eLiixc9BYaAVwQLGoucEF8mo"
+auth_users = "1159334693"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6990313439"
+osowner_users = "1159334693"
 owner_users = [int(num) for num in osowner_users.split(",")]
